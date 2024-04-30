add496c92c4c175775720e17d112370ef378fc16
Syed Abdal SHah <syedahmad123123shah@gmail.com>
2024-04-30 16:29:51 +0500

    stuff

--- lib/pages/home/widgets/bottom_nav_bar.dart
+++ lib/pages/home/widgets/bottom_nav_bar.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:receipe_app/blocs/nav_bar_bloc/cubit/navigation_bar_cubit.dart';
@@ -21,48 +20,56 @@ class AppBottomNavBar extends StatelessWidget {
               InkWell(
                 borderRadius: BorderRadius.circular(15.0),
                 onTap: () => navCubit.updateNavPage(0),
-                child: const Padding(
-                  padding: EdgeInsets.all(8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Image(
-                    image: AssetImage(AppAssets.homeIcon),
+                    image: const AssetImage(AppAssets.homeIcon),
                     height: 30,
-                    color: Colors.white,
+                    color: navCubit.navPageIndex == 0
+                        ? Theme.of(context).colorScheme.primary
+                        : Colors.white,
                   ),
                 ),
               ),
               InkWell(
                 borderRadius: BorderRadius.circular(15.0),
                 onTap: () => navCubit.updateNavPage(1),
-                child: const Padding(
-                  padding: EdgeInsets.all(8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Image(
-                    image: AssetImage(AppAssets.favIcon),
+                    image: const AssetImage(AppAssets.favIcon),
                     height: 30,
-                    color: Colors.white,
+                    color: navCubit.navPageIndex == 1
+                        ? Theme.of(context).colorScheme.primary
+                        : Colors.white,
                   ),
                 ),
               ),
               InkWell(
                 borderRadius: BorderRadius.circular(15.0),
                 onTap: () => navCubit.updateNavPage(2),
-                child: const Padding(
-                  padding: EdgeInsets.all(8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Image(
-                    image: AssetImage(AppAssets.notificationIcon),
+                    image: const AssetImage(AppAssets.notificationIcon),
                     height: 30,
-                    color: Colors.white,
+                    color: navCubit.navPageIndex == 2
+                        ? Theme.of(context).colorScheme.primary
+                        : Colors.white,
                   ),
                 ),
               ),
               InkWell(
                 borderRadius: BorderRadius.circular(15.0),
                 onTap: () => navCubit.updateNavPage(3),
-                child: const Padding(
-                  padding: EdgeInsets.all(8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Image(
-                    image: AssetImage(AppAssets.profileIcon),
+                    image: const AssetImage(AppAssets.profileIcon),
                     height: 30,
-                    color: Colors.white,
+                    color: navCubit.navPageIndex == 3
+                        ? Theme.of(context).colorScheme.primary
+                        : Colors.white,
                   ),
                 ),
               )
@@ -72,4 +79,8 @@ class AppBottomNavBar extends StatelessWidget {
       },
     );
   }
+
+
+
+  
 }
