2266754d667dbdc5aa8bf351e32562e56d7ead88
Syed Abdal SHah <syedahmad123123shah@gmail.com>
2024-05-13 00:51:22 +0500

    stuff

---
+++ lib/utils/app_dialog.dart
@@ -0,0 +1,13 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/material.dart';
+
+class AppDialogs  {
+
+
+
+
+  static customDialog (BuildContext context) async {
+ 
+ AwesomeDialog(context: context ) ;
+  }
+}
