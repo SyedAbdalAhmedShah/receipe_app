036e13cf9dab7ea28ea9cec9919b25bc5437d4ee
Syed Abdal SHah <syedahmad123123shah@gmail.com>
2024-05-14 02:32:30 +0500

    stuff

---
+++ lib/constants/server_strings.dart
@@ -0,0 +1,6 @@
+class ServerStrings {
+  static const String userId = "userId";
+  static const String createdAt = "createdAt";
+  static const String profileUrl = "profileUrl";
+  static const String userName = "userName";
+}
--- lib/repositories/auth_repo.dart
+++ lib/repositories/auth_repo.dart
@@ -3,30 +3,36 @@ import 'dart:developer';
 import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart';
 import 'package:receipe_app/config/server_config.dart';
+import 'package:receipe_app/constants/server_strings.dart';
 import 'package:receipe_app/dependency_injection/server_client.dart';
 import 'package:receipe_app/utils/dependency.dart';
 
 class AuthRepository {
   ServerClient serverClient = serviceLocator.get<ServerClient>();
-  Future createEmailPassword({required String email, required String password, required String userName}) async {
-    User user =
-        await serverClient.account.create(userId: ID.unique(), email: email, password: password, name: userName);
+  Future createEmailPassword(
+      {required String email,
+      required String password,
+      required String userName}) async {
+    User user = await serverClient.account.create(
+        userId: ID.unique(), email: email, password: password, name: userName);
 
     log("TOEKN DATA ${user.toMap()} === ${user} ");
-    // Document document = await serverClient.databases.createDocument(
-    //     databaseId: ServerConfig.recipeDatabaseId,
-    //     collectionId: ServerConfig.userCollectionId,
-    //     documentId: ID.unique(),
-    //     data: {"userId": user.$id.toString()});
+    Document document = await serverClient.databases.createDocument(
+        databaseId: ServerConfig.recipeDatabaseId,
+        collectionId: ServerConfig.userCollectionId,
+        documentId: ID.unique(),
+        data: {
+          ServerStrings.userId: user.$id.toString(),
+          ServerStrings.userName: userName,
+          ServerStrings.createdAt: user.$createdAt.toString()
+        });
+        log("UPLOADED DOCUMENT ${document.data}");
   }
 
   Future signInWithEmailAndPassword({
     required String email,
     required String password,
   }) async {
-    // User user = await serverClient.account.get();
-    // await serverClient.account.deleteSessions();
-
     Session session = await serverClient.account.createEmailPasswordSession(
       email: email,
       password: password,
