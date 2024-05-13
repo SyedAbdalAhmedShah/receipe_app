1fe6fd6737628ebba9307d98a21580b8adcebc94
Syed Abdal SHah <syedahmad123123shah@gmail.com>
2024-05-13 23:19:37 +0500

    stuff

--- lib/config/server_config.dart
+++ lib/config/server_config.dart
@@ -1,3 +1,5 @@
 class ServerConfig {
   static const String projectId = "662d029d002952b12a38";
+  static const String recipeDatabaseId = "663a732a0007f78c7d69";
+  static const String userCollectionId = "66423f64001a67aebc41";
 }
--- lib/repositories/auth_repo.dart
+++ lib/repositories/auth_repo.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart';
+import 'package:receipe_app/config/server_config.dart';
 import 'package:receipe_app/dependency_injection/server_client.dart';
 import 'package:receipe_app/utils/dependency.dart';
 
@@ -12,6 +13,11 @@ class AuthRepository {
         await serverClient.account.create(userId: ID.unique(), email: email, password: password, name: userName);
 
     log("TOEKN DATA ${user.toMap()} === ");
+    Document document = await serverClient.databases.createDocument(
+        databaseId: ServerConfig.recipeDatabaseId,
+        collectionId: ServerConfig.userCollectionId,
+        documentId: ID.unique(),
+        data: {"userId": user.$id.toString()});
   }
 
   Future signInWithEmailAndPassword({
@@ -25,7 +31,9 @@ class AuthRepository {
       email: email,
       password: password,
     );
-    // // log("USER ${user.toMap()}");
+    User user = await serverClient.account.get();
+    log("USER ${user.toMap()}");
+
     log("DEVICE INFO ${session.deviceBrand} == ${session.deviceModel} == ${session.deviceName} == ${session.userId}");
   }
 
