93407ed5174a548dcabbef9f934504f8242b793a
Syed Abdal SHah <syedahmad123123shah@gmail.com>
2024-05-19 00:12:07 +0500

    bloc package added

--- lib/blocs/home_bloc/bloc/home_bloc.dart
+++ lib/blocs/home_bloc/bloc/home_bloc.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'home_event.dart';
@@ -6,7 +6,7 @@ part 'home_state.dart';
 part 'home_bloc.freezed.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
-  HomeBloc() : super(_Initial()) {
+  HomeBloc() : super(const _Initial()) {
     on<HomeEvent>((event, emit) {
       // TODO: implement event handler
     });
