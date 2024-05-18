0b4043ef6341d4575cf6bc77debeeda418a078d6
Syed Abdal SHah <syedahmad123123shah@gmail.com>
2024-05-18 23:53:10 +0500

    stuff

--- lib/widgets/profile_picture.dart
+++ lib/widgets/profile_picture.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:receipe_app/blocs/nav_bar_bloc/cubit/navigation_bar_cubit.dart';
 import 'package:receipe_app/constants/app_assets.dart';
 import 'package:receipe_app/constants/app_colors.dart';
 
@@ -8,12 +10,16 @@ class ProfilePictureSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
-    return Container(
-      height: size.height * 0.05,
-      decoration: BoxDecoration(
-          color: AppColor.profileBackgroundColor,
-          borderRadius: BorderRadius.circular(10.0)),
-      child: const Image(image: AssetImage(AppAssets.dummyProfile)),
+    final navCubit = context.read<NavigationBarCubit>();
+    return InkWell(
+      onTap: () => navCubit.updateNavPage(3),
+      child: Container(
+        height: size.height * 0.05,
+        decoration: BoxDecoration(
+            color: AppColor.profileBackgroundColor,
+            borderRadius: BorderRadius.circular(10.0)),
+        child: const Image(image: AssetImage(AppAssets.dummyProfile)),
+      ),
     );
   }
 }
