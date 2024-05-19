855096043cea05f73228802e0470a4a2021aa553
Syed Abdal SHah <syedahmad123123shah@gmail.com>
2024-05-20 02:50:29 +0500

    loading widget added

--- lib/constants/server_strings.dart
+++ lib/constants/server_strings.dart
@@ -3,6 +3,8 @@ class ServerStrings {
   static const String createdAt = "createdAt";
   static const String profileUrl = "profileUrl";
   static const String userName = "userName";
+  static const String rapidApiKey = "X-RapidAPI-Key";
+  static const String rapidApiHost = "X-RapidAPI-Host";
 
   // SharedPrefrences Key //
   static const String userDataKey = "userData";
--- lib/repositories/home_repo.dart
+++ lib/repositories/home_repo.dart
@@ -1,13 +1,14 @@
 import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:receipe_app/config/server_config.dart';
+import 'package:receipe_app/constants/server_strings.dart';
 import 'package:receipe_app/model/prodcut/product_model.dart';
 
 class HomeRepository {
   final dio = Dio();
   final headers = {
-    "X-RapidAPI-Key": ServerConfig.rapidApiKey,
-    "X-RapidAPI-Host": ServerConfig.rapidApiHost,
+    ServerStrings.rapidApiKey: ServerConfig.rapidApiKey,
+    ServerStrings.rapidApiHost: ServerConfig.rapidApiHost,
   };
   Future getAllDishes() async {
     final response = await dio.get(
@@ -16,7 +17,8 @@ class HomeRepository {
     );
     final responseList = response.data as List;
     log(responseList.toString());
-    List<ProductModel> products =responseList.map((e) =>  ProductModel.fromJson(e)).toList();
+    List<ProductModel> products =
+        responseList.map((e) => ProductModel.fromJson(e)).toList();
     log(products.length.toString());
   }
 }
