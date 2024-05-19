356d56cb6631b8febd34bbd582725026e2631726
Syed Abdal SHah <syedahmad123123shah@gmail.com>
2024-05-20 02:44:10 +0500

    api. data convert into model

--- lib/repositories/home_repo.dart
+++ lib/repositories/home_repo.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:receipe_app/config/server_config.dart';
+import 'package:receipe_app/model/prodcut/product_model.dart';
 
 class HomeRepository {
   final dio = Dio();
@@ -13,6 +14,9 @@ class HomeRepository {
       ServerConfig.baseUrl,
       options: Options(headers: headers),
     );
-    log("data length ${response.data}");
+    final responseList = response.data as List;
+    log(responseList.toString());
+    List<ProductModel> products =responseList.map((e) =>  ProductModel.fromJson(e)).toList();
+    log(products.length.toString());
   }
 }
