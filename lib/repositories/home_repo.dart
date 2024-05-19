5c7d8f3870e0678364eeb1afee4e9391bad4bf83
Syed Abdal SHah <syedahmad123123shah@gmail.com>
2024-05-20 01:10:01 +0500

    api call test added

--- lib/repositories/home_repo.dart
+++ lib/repositories/home_repo.dart
@@ -1,17 +1,18 @@
+import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:receipe_app/config/server_config.dart';
-import 'package:receipe_app/server/server_constants.dart';
 
 class HomeRepository {
-
   final dio = Dio();
-  Future getAllDishes () async {
-    dio.options = 
-    BaseOptions(baseUrl: ServerConfig.baseUrl, connectTimeout:const Duration( seconds: 10),headers: {
-"X-RapidAPI-Key" :  ServerConfig.rapidApiKey,
-"X-RapidAPI-Host" : ServerConfig.rapidApiHost,
-    });
-    final response = dio.get(url)
-
+  final headers = {
+    "X-RapidAPI-Key": ServerConfig.rapidApiKey,
+    "X-RapidAPI-Host": ServerConfig.rapidApiHost,
+  };
+  Future getAllDishes() async {
+    final response = await dio.get(
+      ServerConfig.baseUrl,
+      options: Options(headers: headers),
+    );
+    log("data length ${response.data}");
   }
 }
